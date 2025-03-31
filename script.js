f787d8b282487347fad82931a70cf7c5378f5f7f
unknown <fowosere>
2025-03-31 03:56:42 +0100

    third commit with updates on me

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ const profiles = {
     address: 'Business Management Suite, Barton Dock Road, M17 8AA',
     website: 'N/A',
     initials: 'JP',
-    avatarUrl: 'me.JPEG'
+    avatarUrl: 'me.jpg'
   }
 };
 
